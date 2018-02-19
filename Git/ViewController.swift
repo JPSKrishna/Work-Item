9c9e4a1b561155cbd849c978c168225a4bac7873
Krishna Maurya <krishnamaurya@Krishnas-MacBook-Pro.local>
2018-02-19 16:28:01 +0530

    Initial commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Krishna Maurya on 19/02/18.
+//  Copyright © 2018 Aktatech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello, World!")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
